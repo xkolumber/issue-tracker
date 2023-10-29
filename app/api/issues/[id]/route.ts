26eb96748bfbead7d01c3e3255e4cc1e55a49a39
Lubos Kolumber <lubosk158@gmail.com>
2023-10-29 11:28:27 +0100

    Enhance the API to assign issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,58 +1,92 @@
-import authOptions from '@/app/auth/AuthOptions';
-import { issueSchema } from '@/app/validationSchemas';
-import prisma from '@/prisma/client';
-import { getServerSession } from 'next-auth';
-import { NextRequest, NextResponse } from 'next/server';
+import authOptions from "@/app/auth/AuthOptions";
+import { patchIssueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
-    request: NextRequest,
-    {params}: {params: {id: string}}){
-        const session = getServerSession(authOptions);
-        if (!session)
-            return NextResponse.json({}, {status: 401})
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
+
+  const body = await request.json();
+  const validation = patchIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), {
+      status: 400,
+    });
+
+
+
+  const { assignedToUserId, title, description } = body;
+
+  console.log('Request body:', body);
+  console.log('AssignedToUserId:', assignedToUserId);
+
+  if (assignedToUserId) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: assignedToUserId },
+      });
+      if (!user) {
+        return NextResponse.json(
+          { error: `User with ID ${assignedToUserId} not found.` },
+          { status: 400 }
+        );
+      }
+    } catch (error) {
+      console.error('Error querying user:', error);
+      return NextResponse.json(
+        { error: 'An error occurred while querying the user.' },
+        { status: 500 }
+      );
+    }
     
-    const body = await request.json();
+  }
 
-    const validation = issueSchema.safeParse(body);
-    if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400})
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json(
+      { error: "Invalid issue" },
+      { status: 404 }
+    );
 
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    })
-    if(!issue)
-        return NextResponse.json({error: 'Invalid issue'}, {status:404})
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title,
+      description,
+      assignedToUserId
+    },
+  });
 
-    const updatedIssue = await prisma.issue.update({
-        where:{ id: issue.id},
-        data: {
-            title: body.title,
-            description: body.description
-        }
-    })
-
-    return NextResponse.json(updatedIssue)
+  return NextResponse.json(updatedIssue);
 }
 
-
 export async function DELETE(
-    request: NextRequest,
-    {params}: {params: {id: string}}){
-        const session = getServerSession(authOptions);
-        if (!session)
-            return NextResponse.json({}, {status: 401})
-    
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
 
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    });
-    if(!issue)
-        return NextResponse.json({error: 'Invalid issue'}, {status : 404})
-    
-    await prisma.issue.delete({
-        where: {
-            id: issue.id
-        }
-    })
-    return NextResponse.json({})
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json(
+      { error: "Invalid issue" },
+      { status: 404 }
+    );
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({});
 }
---
+++ prisma/migrations/20231028223121_deleting_all/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `issue` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verificationtoken` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `account`;
+
+-- DropTable
+DROP TABLE `issue`;
+
+-- DropTable
+DROP TABLE `session`;
+
+-- DropTable
+DROP TABLE `user`;
+
+-- DropTable
+DROP TABLE `verificationtoken`;
